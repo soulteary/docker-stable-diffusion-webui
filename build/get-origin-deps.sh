b0d2966ddcba7cf12e0453e4b6cc61761685dc4c
Su Yang <soulteary@gmail.com>
2024-01-16 14:49:59 +0800

    chore: chmod +x

--- build/get-origin-deps.sh
+++ build/get-origin-deps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TEMP_DIR=temp
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/common.sh"
 
 mkdir -p "./${TEMP_DIR}"
-curl -L -o "./${TEMP_DIR}/launch_utils.py" https://github.com/AUTOMATIC1111/stable-diffusion-webui/raw/master/modules/launch_utils.py
+curl -L -o "${DEPS_FILE}" https://github.com/AUTOMATIC1111/stable-diffusion-webui/raw/master/modules/launch_utils.py
+echo "Downloaded ${DEPS_FILE}"
