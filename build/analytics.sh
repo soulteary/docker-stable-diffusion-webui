41d9e80a2f51bc400fd73343593863dad1d6033d
Su Yang <soulteary@gmail.com>
2024-01-16 15:11:06 +0800

    chore: add build/analytics(wip)

---
+++ build/analytics.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "${SCRIPT_DIR}/common.sh"
+
+PACKAGES_DIR="./packages"
+mkdir -p "${PACKAGES_DIR}"
+
+# clip_package
+# =================
+echo "[GET Info] clip_package"
+clip_package_info=$(cat ${DEPS_FILE} | grep ".get('CLIP_PACKAGE'" | grep -oP "(?<=\").*(?=\")")
+echo $clip_package_info
+clip_commit_hash=$(echo $clip_package_info | grep -oP '(?<=archive/)\w+(?=\.zip)')
+echo $clip_commit_hash
+if [[ "${clip_package_info}" != "${CLIP_PACKAGE}" ]]; then
+    echo "clip_commit_hash is not equal to CLIP_PACKAGE, need upgrade."
+    echo "please report this issue to https://github.com/soulteary/docker-stable-diffusion-webui/"
+    exit 1
+fi
+if [[ "${CLIP_COMMIT_HASH}" != "" ]]; then
+    echo "recommend CLIP_COMMIT_HASH is not empty, use it. ${CLIP_COMMIT_HASH}"
+    clip_commit_hash="${CLIP_COMMIT_HASH}"
+fi
+if [[ -d "${PACKAGES_DIR}/clip" ]]; then
+    echo "clip_package is already exist, skip clone."
+else
+    git clone "${CLIP_GITHUB}" "${PACKAGES_DIR}/clip"
+fi
+cd "${PACKAGES_DIR}/clip"
+git checkout "${clip_commit_hash}"
+cd "../../"
+echo ""
+# =================
+
+
+# openclip_package
+# =================
+echo "get openclip_package info"
+info=$(cat ${DEPS_FILE} | grep ".get('OPENCLIP_PACKAGE'" | grep -oP "(?<=\").*(?=\")")
+echo $info
+openclip_commit_hash=$(echo $info | grep -oP '(?<=archive/)\w+(?=\.zip)')
+echo $openclip_commit_hash
+
+
+# stable_diffusion_repo
+echo "get stable_diffusion_repo info"
+info=$(cat ${DEPS_FILE} | grep ".get('STABLE_DIFFUSION_REPO'" | grep -oP "(?<=\").*(?=\")")
+echo $info
+
+stable_diffusion_commit_hash=$(cat ${DEPS_FILE} | grep ".get('STABLE_DIFFUSION_COMMIT_HASH'" | grep -oP "(?<=\").*(?=\")")
+echo $stable_diffusion_commit_hash
+
+# stable_diffusion_xl_repo
+echo "get stable_diffusion_xl_repo info"
+info=$(cat ${DEPS_FILE} | grep ".get('STABLE_DIFFUSION_XL_REPO'" | grep -oP "(?<=\").*(?=\")")
+echo $info
+
+stable_diffusion_xl_commit_hash=$(cat ${DEPS_FILE} | grep ".get('STABLE_DIFFUSION_XL_COMMIT_HASH'" | grep -oP "(?<=\").*(?=\")")
+echo $stable_diffusion_xl_commit_hash
+
+
+
+
+# k_diffusion_repo
+echo "get k_diffusion_repo info"
+info=$(cat ${DEPS_FILE} | grep ".get('K_DIFFUSION_REPO'" | grep -oP "(?<=')[^']+(?=')")
+echo $info
+
+
+k_diffusion_commit_hash=$(cat ${DEPS_FILE} | grep ".get('K_DIFFUSION_COMMIT_HASH'" | grep -oP "(?<=\").*(?=\")")
+echo $k_diffusion_commit_hash
+
+
+
+
+# codeformer_repo
+echo "get codeformer_repo info"
+info=$(cat ${DEPS_FILE} | grep ".get('CODEFORMER_REPO'" | grep -oP "(?<=')[^']+(?=')")
+echo $info
+
+
+codeformer_commit_hash=$(cat ${DEPS_FILE} | grep ".get('CODEFORMER_COMMIT_HASH'" | grep -oP "(?<=\").*(?=\")")
+echo $codeformer_commit_hash
+
+
+# blip_repo
+echo "get blip_repo info"
+info=$(cat ${DEPS_FILE} | grep ".get('BLIP_REPO'" | grep -oP "(?<=')[^']+(?=')")
+echo $info
+
+
+blip_commit_hash=$(cat ${DEPS_FILE} | grep ".get('BLIP_COMMIT_HASH'" | grep -oP "(?<=\").*(?=\")")
+echo $blip_commit_hash
+
--- build/common.sh
+++ build/common.sh
@@ -2,3 +2,25 @@
 
 TEMP_DIR=temp
 DEPS_FILE="./${TEMP_DIR}/launch_utils.py"
+
+# Sync with upstream repo
+CLIP_PACKAGE="https://github.com/openai/CLIP/archive/d50d76daa670286dd6cacf3bcd80b5e4823fc8e1.zip"
+OPENCLIP_PACKAGE="https://github.com/mlfoundations/open_clip/archive/bb6e834e9c70d9c27d0dc3ecedeebeaeb1ffad6b.zip"
+CLIP_GITHUB="https://github.com/openai/CLIP.git"
+CLIP_COMMIT_HASH="a1d071733d7111c9c014f024669f959182114e33"
+
+STABLE_DIFFUSION_REPO="https://github.com/Stability-AI/stablediffusion.git"
+STABLE_DIFFUSION_COMMIT_HASH="cf1d67a6fd5ea1aa600c4df58e5b47da45f6bdbf"
+
+STABLE_DIFFUSION_XL_REPO="https://github.com/Stability-AI/generative-models.git"
+STABLE_DIFFUSION_XL_COMMIT_HASH="45c443b316737a4ab6e40413d7794a7f5657c19f"
+
+K_DIFFUSION_REPO=https://github.com/crowsonkb/k-diffusion.git
+K_DIFFUSION_COMMIT_HASH="ab527a9a6d347f364e3d185ba6d714e22d80cb3c"
+
+CODEFORMER_REP="https://github.com/sczhou/CodeFormer.git"
+CODEFORMER_COMMIT_HASH="c5b4593074ba6214284d6acd5f1719b6c5d739af"
+
+BLIP_REPO="https://github.com/salesforce/BLIP.git"
+BLIP_COMMIT_HASH="48211a1594f1321b00f14c9f7a5b4813144b2fb9"
+
