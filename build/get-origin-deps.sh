4681c61cfc345af42eab2f5e1869a070ec4d3a06
Su Yang <soulteary@gmail.com>
2024-01-17 23:24:20 +0800

    fix: use correct branch

--- build/get-origin-deps.sh
+++ build/get-origin-deps.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${SCRIPT_DIR}/common.sh"
 
 mkdir -p "./${TEMP_DIR}"
-curl -L -o "${DEPS_FILE}" "${STABLE_DIFFUSION_WEBUI_REPO}/raw/master/modules/launch_utils.py"
+curl -L -o "${DEPS_FILE}" "${STABLE_DIFFUSION_WEBUI_REPO}/raw/main/modules/launch_utils.py"
 echo "Downloaded ${DEPS_FILE}"
