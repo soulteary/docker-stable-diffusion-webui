441cffba9690b0688e7f233c65a1ade1a9c161af
Su Yang <soulteary@gmail.com>
2024-01-17 22:05:56 +0800

    chore: analytics deps from soulteary/stable-diffusion-webui

--- build/get-origin-deps.sh
+++ build/get-origin-deps.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${SCRIPT_DIR}/common.sh"
 
 mkdir -p "./${TEMP_DIR}"
-curl -L -o "${DEPS_FILE}" https://github.com/AUTOMATIC1111/stable-diffusion-webui/raw/master/modules/launch_utils.py
+curl -L -o "${DEPS_FILE}" "${STABLE_DIFFUSION_WEBUI_REPO}/raw/master/modules/launch_utils.py"
 echo "Downloaded ${DEPS_FILE}"
