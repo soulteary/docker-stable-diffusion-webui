450778576d4cf6ab086d9d6ddd61fb6ca2f37952
Su Yang <soulteary@gmail.com>
2024-01-18 21:28:40 +0800

    chore: update mirror deps file

--- build/get-origin-deps.sh
+++ build/get-origin-deps.sh
@@ -4,5 +4,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "${SCRIPT_DIR}/common.sh"
 
 mkdir -p "./${TEMP_DIR}"
-curl -L -o "${DEPS_FILE}" "${STABLE_DIFFUSION_WEBUI_REPO}/raw/main/modules/launch_utils.py"
+
+if [ "$USE_CHINA_MIRROR" = "true" ]; then
+    curl -L -o "${DEPS_FILE}"  "https://modelscope.cn/api/v1/models/soulteary/sd-web-deps/repo?Revision=master&FilePath=launch_utils.py"
+else
+    curl -L -o "${DEPS_FILE}" "${STABLE_DIFFUSION_WEBUI_REPO}/raw/main/modules/launch_utils.py"
+fi
+
 echo "Downloaded ${DEPS_FILE}"
