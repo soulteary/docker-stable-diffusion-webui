3dabfac7e77f1ba0bd3bd02eb7a33e816907e3bf
Su Yang <soulteary@gmail.com>
2024-01-18 20:29:53 +0800

    docs: update models download

--- build/get-animatediff-models.sh
+++ build/get-animatediff-models.sh
@@ -8,14 +8,18 @@ if [[ -d "${MODELS_DIR}/guoyww/animatediff" ]]; then
 else
     echo "[GET Models] guoyww/animatediff"
     if [ "$USE_CHINA_MIRROR" = "true" ]; then
-        pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-        pip install modelscope
-        python3 -c "from modelscope import snapshot_download;snapshot_download('Shanghai_AI_Laboratory/animatediff', cache_dir='./models/')"
         mkdir -p "${MODELS_DIR}/guoyww/animatediff"
-        mv "${MODELS_DIR}/Shanghai_AI_Laboratory/animatediff/v2_lora_*" "${MODELS_DIR}/guoyww/animatediff/"
-        mv "${MODELS_DIR}/Shanghai_AI_Laboratory/animatediff/mm_sd_v15_v2.ckpt" "${MODELS_DIR}/guoyww/animatediff/"
-        mv "${MODELS_DIR}/Shanghai_AI_Laboratory/animatediff/mm_sdxl_v10_beta.ckpt" "${MODELS_DIR}/guoyww/animatediff/"
-        rm -rf "${MODELS_DIR}/Shanghai_AI_Laboratory"
+        # https://modelscope.cn/models/Shanghai_AI_Laboratory/animatediff
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/mm_sd_v15_v2.ckpt"           "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=mm_sd_v15_v2.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/mm_sdxl_v10_beta.ckpt"       "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=mm_sdxl_v10_beta.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_PanLeft.ckpt"        "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_PanLeft.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_PanRight.ckpt"       "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_PanRight.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_RollingAnticlockwise.ckpt"   "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_RollingAnticlockwise.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_RollingClockwise.ckpt"       "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_RollingClockwise.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_TiltDown.ckpt"       "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_TiltDown.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_TiltUp.ckpt"         "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_TiltUp.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_ZoomIn.ckpt"         "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_ZoomIn.ckpt"
+        curl -L -o "${MODELS_DIR}/guoyww/animatediff/v2_lora_ZoomOut.ckpt"       "https://modelscope.cn/api/v1/models/Shanghai_AI_Laboratory/animatediff/repo?Revision=master&FilePath=v2_lora_ZoomOut.ckpt"
     else
         pip install huggingface_hub[cli]
         huggingface-cli download guoyww/animatediff mm_sd_v15_v2.ckpt --local-dir=./models/guoyww/animatediff --cache-dir=./cache --local-dir-use-symlinks=False --resume-download
